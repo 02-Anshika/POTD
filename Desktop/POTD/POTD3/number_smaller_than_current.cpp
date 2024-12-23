7f974f1b3d29914d43aceadb3eda5a585d57e4ee
02-Anshika <anshikakesri02@gmail.com>
2024-12-23 16:35:43 +0530

    day3

---
+++ Desktop/POTD/POTD3/number_smaller_than_current.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+       vector<int> ans;
+        int n=nums.size(),c;
+        for(int i=0;i<n;i++)
+        { c=0;
+           
+           for(int j=0;j<n;j++)
+           { 
+             if(nums[i]>nums[j])
+             c++;
+           }
+           ans.push_back(c);
+        }
+        return ans;
+
+        //sort(nums.begin(),nums.end());
+
+    }
+};
